c8a152f012f85ebd66d98378fb92606fd0a1cf28
MoesioMarcelino <moesiomarcelino1@gmail.com>
2022-01-04 23:16:23 -0300

    feat: create checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-checkbox">
+    <input type="checkbox" name="remember" id="remember" :v-model="value" />
+    <label class="label-checkbox" for="remember">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Checkbox",
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
+
+<style>
+.container-checkbox {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+input {
+  height: 24px;
+  width: 24px;
+}
+
+.label-checkbox {
+  margin-left: 16px;
+  cursor: pointer;
+}
+</style>

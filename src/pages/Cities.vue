b59c34719405c34d42e4a0d5f245c4d68087e4a2
MoesioMarcelino <moesiomarcelino1@gmail.com>
2022-01-09 14:42:32 -0300

    feat: add db.json and use card city component

--- src/pages/Cities.vue
+++ src/pages/Cities.vue
@@ -4,16 +4,29 @@
       <Title label="Cidades" />
       <PrimaryButton label="+ Adicionar um perfil" type="button" />
     </nav>
+
+    <div class="container-card-cities">
+      <CardCity />
+      <CardCity />
+      <CardCity />
+      <CardCity />
+      <CardCity />
+      <CardCity />
+      <CardCity />
+      <CardCity />
+      <CardCity />
+    </div>
   </div>
 </template>
 
 <script>
 import Title from "../components/Title.vue"
 import PrimaryButton from "../components/PrimaryButton.vue"
+import CardCity from "../components/CardCity.vue"
 
 export default {
   name: "Cities",
-  components: { Title, PrimaryButton },
+  components: { Title, PrimaryButton, CardCity },
 }
 </script>
 
@@ -22,15 +35,24 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
   padding: 24px 40px;
   background-color: #fff;
   border: 1px solid #dce2e6;
-  column-gap: 24px;
+  column-gap: 48px;
+  row-gap: 12px;
 }
 
 nav button {
   width: fit-content;
   height: 48px;
   padding: 12px 24px;
+}
+
+.container-card-cities {
+  padding: 40px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
 }
 </style>
